a1a6e92caa9769549772204814eab66adf7bf95d
vedantkavhar <vedant.22110832@viit.ac.in>
2025-09-17 13:06:34 +0530

    step 1.2 app.use

--- src/app.js
+++ src/app.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const app=express();
 
-app.use("/",(req,res)=>{
-    res.send("hello from http express server");
+//this will handle anythin comes after /,/abc/nko,/hello 
+// /anything 
+// app.use("/",(req,res)=>{
+//     res.send("first for /");
+// })
+
+
+//this will hanlde naything comes after /hello,/hello/123
+// /hello/anything 
+app.use("/hello",(req,res)=>{
+    res.send("second for /hello");
 })
 
-app.use("/hello",(req,res)=>{
-    res.send("this is hello route 2n d handler");
+//this will handle anything comes after /test,/test/123
+// /test/anything
+app.use("/test",(req,res)=>{
+    res.send("third test");
 })
+
+
 
 
 app.listen(7777,()=>{
